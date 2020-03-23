e0ed11e146124dc84094263e0120137b10528a40
SJH <shenjunhong@meizu.com>
2020-03-23 18:40:42 +0800

    增加了不同路径

---
+++ shenjunhong/week2-dynamic/不同路径.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+
+};
+
+var val =uniquePaths(m = 7, n = 3)  
+//28
+
+1 <= m, n <= 100;
+2 * 10 ^ 9;
