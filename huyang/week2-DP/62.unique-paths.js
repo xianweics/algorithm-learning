38f1e9e333c5f16fb9a97a71c79160b859ac7c56
胡阳 <yang.hu01@hand-china.com>
2020-03-29 17:31:57 +0800

    week3-dp

--- huyang/week2-DP/62.unique-paths.js
+++ huyang/week2-DP/62.unique-paths.js
@@ -2,6 +2,8 @@
 // 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
 // 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
 // 排列组合：向右m-1步，向下n-1步，一共m+n-2，其中（m-1）或（n-1）步特殊，即C(m+n-2,m-1)。
+// 时间复杂度：1
+// 空间复杂度：1
 var uniquePaths = function(m, n) {
     if(m == 1 || n == 1)    return 1;
     let a = m+n-2;
@@ -14,3 +16,4 @@ var uniquePaths = function(m, n) {
     }
     return p/q;
 };
+uniquePaths(4,4);
