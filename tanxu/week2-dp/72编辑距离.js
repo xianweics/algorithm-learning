146b348dd4450a254f68e99a20ae2ccd496cb4ef
tanxu <18602863283@163.com>
2020-03-29 22:05:03 +0800

    第二周-动态规划代码笔记提交

--- tanxu/week2-dp/72编辑距离.js
+++ tanxu/week2-dp/72编辑距离.js
@@ -8,7 +8,32 @@
 //
 // 其中，dp[i-1][j-1] 表示替换操作，dp[i-1][j] 表示删除操作，dp[i][j-1] 表示插入操作。
 
-
 var minDistance = function(word1, word2) {
-
+  // 特殊处理占位
+  word1 = '*'+word1
+  word2 = '*'+word2
+  let dp = []
+  dp[0] = []
+  dp[0][0] = 0
+  for (let i = 0; i <= word1.length; i++) {
+    dp[i] = []
+    for (let j = 0; j <= word2.length; j++) {
+      if (i === 0) {
+        dp[i][j] = j
+        continue
+      }
+      if (j === 0) {
+        dp[i][j] = i
+        continue
+      }
+      if (word1[i] === word2[j]) {
+        dp[i][j] = dp[i-1][j-1]
+      } else {
+        dp[i][j] = Math.min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
+      }
+    }
+  }
+  return dp[word1.length][word2.length]
 };
+
+console.log(minDistance('horse','ros'))
