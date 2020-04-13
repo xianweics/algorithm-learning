488ffdcdfd4d6dcce39e27c4bb207fa9961d88cb
Qiner Liu <38338800+Qiner0900@users.noreply.github.com>
2020-04-13 23:38:35 +0800

    添加复杂度

--- liuqin/week3-string/28.strStr.js
+++ liuqin/week3-string/28.strStr.js
@@ -1,13 +1,15 @@
 /**
  * 执行用时：56 ms
  * 内存消耗：35 MB
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(1)
  * @param {string} haystack
  * @param {string} needle
  * @return {number}
  */
 var strStr = function(haystack, needle) {
   let result = -1;
-  console.log(haystack,needle);
+//   console.log(haystack,needle);
   if (!needle){
       result = 0;
      return result;
@@ -21,13 +23,12 @@ var strStr = function(haystack, needle) {
       }
       
       for(let i=0;i<len-needlen+1;i++){
-
-              if(haystack.slice(i,needlen+i) == needle){
+        if(haystack.slice(i,needlen+i) == needle){
             result = i;
             return result;
-}
+        }
       }
-  return result;//未找到符合条件的字符串    
+      return result;//未找到符合条件的字符串    
   }
 
 };
